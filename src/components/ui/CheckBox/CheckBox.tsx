f2c1e497704db7e00e7d19f29af13fed5131fd5b
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-08-21 17:19:16 +0300

    feat:add Checkbox

---
+++ src/components/ui/CheckBox/CheckBox.tsx
@@ -0,0 +1,38 @@
+'use client'
+import * as Checkbox from '@radix-ui/react-checkbox';
+import Ok from './ok-sq.svg'
+import Image from 'next/image'
+import s from './CheckBox.module.css'
+
+type Props = {
+  checked: boolean,
+  onChange: (checked: boolean) => void,
+  text: string,
+}
+
+/**
+ * @example
+ * Принимает пропсы калбэка и стейта
+ * <CheckBox
+ * checked={checked}
+ * onChange={changeChecked}
+ * text={"любой текст"}/>
+ */
+export const CheckBox = ({checked, onChange, text}: Props) => {
+  return (
+    <div>
+      <Checkbox.Root className={s.checkboxRoot} checked={checked} onCheckedChange={onChange}>
+        <div className={s.checkBox}>
+          <Checkbox.Indicator>
+            <Image src={Ok} alt="" className={s.s}/>
+            <div className={s.bgIndicator}/>
+          </Checkbox.Indicator>
+        </div>
+      </Checkbox.Root>
+      <label htmlFor="c1" className={'text-small'}>
+        {text}
+      </label>
+    </div>
+  );
+};
+
