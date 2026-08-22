165c143999111ea850b1e566e919ea2c62a36992
IlyaGrigor <birthdayay371@gmail.com>
2026-08-22 11:09:04 +0300

    feat: add Alert component

---
+++ src/components/ui/Alert/Alert.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React from 'react'
+import s from './Alert.module.css'
+import CloseIcon from '../../icons/CloseIcon/CloseIcon'
+
+type AlertType = {
+  variant: 'error' | 'success'
+  text: string
+  onClose: () => void
+}
+
+const Alert = ({ variant, text, onClose }: AlertType) => {
+  return (
+    <div className={`${s.alert} ${s[variant]}`}>
+      <div className={s.message}>
+        {variant === 'error' && <strong className={s.errorTitle}>Error! </strong>}
+        {text}
+      </div>
+      <button className={s.closeButton} type="button" onClick={onClose} aria-label="Close alert">
+        <CloseIcon />
+      </button>
+    </div>
+  )
+}
+
+export default Alert
