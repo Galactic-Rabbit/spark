b29c75d0c12948626147325a920bae053efb09fd
IlyaGrigor <birthdayay371@gmail.com>
2026-08-23 14:57:26 +0300

    fix: update Alert component

--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -5,23 +5,24 @@ import s from './Alert.module.css'
 import CloseIcon from '../../icons/CloseIcon/CloseIcon'
 
 type AlertType = {
-  variant: 'error' | 'success'
-  text: string
-  onClose: () => void
+    variant: 'error' | 'success'
+    text: string
+    onClose?: () => void
+    strong: string
 }
 
-const Alert = ({ variant, text, onClose }: AlertType) => {
-  return (
-    <div className={`${s.alert} ${s[variant]}`}>
-      <div className={s.message}>
-        {variant === 'error' && <strong className={s.errorTitle}>Error! </strong>}
-        {text}
-      </div>
-      <button className={s.closeButton} type="button" onClick={onClose} aria-label="Close alert">
-        <CloseIcon />
-      </button>
-    </div>
-  )
+const Alert = ({variant, text, onClose, strong}: AlertType) => {
+    return (
+        <div className={`${s.alert} ${s[variant]}`}>
+            <div className={s.message}>
+                {variant === 'error' && <strong className={s.errorTitle}>{strong} </strong>}
+                {text}
+            </div>
+            <button className={s.closeButton} type="button" onClick={onClose} aria-label="Close alert">
+                <CloseIcon/>
+            </button>
+        </div>
+    )
 }
 
 export default Alert
