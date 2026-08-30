a3c1e5527faacf908d7b10b09552bf022f7acbfb
BalandinaM <marina.balandina.89@mail.ru>
2026-08-31 00:14:20 +0500

    поправила стили формы регистрации (увлеклась слегка=))

--- src/screens/SignUpPage/SignUpPage.tsx
+++ src/screens/SignUpPage/SignUpPage.tsx
@@ -1,6 +1,10 @@
 import { AuthFormWrapper } from '@/features/auth/components/AuthFormWrapper'
-import {RegisterForm} from "@/features/auth/components/RegisterForm";
+import { RegisterForm } from '@/features/auth/components/RegisterForm'
 
 export const SignUpPage = () => {
-  return <RegisterForm/>
+  return (
+    <AuthFormWrapper>
+      <RegisterForm />
+    </AuthFormWrapper>
+  )
 }
