466ef17ee254234aa93eeeb4ae1c8bdc2b5e70e7
IlyaGrigor <birthdayay371@gmail.com>
2026-09-01 20:18:27 +0300

    fix: <strong> value in Alert now is hardcoded

--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -8,14 +8,13 @@ type AlertType = {
   variant: 'error' | 'success'
   text: string
   onClose?: () => void
-  strong: string
 }
 
-const Alert = ({ variant, text, onClose, strong }: AlertType) => {
+const Alert = ({ variant, text, onClose }: AlertType) => {
   return (
     <div className={`${s.alert} ${s[variant]}`}>
       <div className={s.message}>
-        {variant === 'error' && <strong className={s.errorTitle}>{strong} </strong>}
+        {variant === 'error' && <strong className={s.errorTitle}>Error!</strong>}
         {text}
       </div>
       <button className={s.closeButton} type="button" onClick={onClose} aria-label="Close alert">
